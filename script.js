b75d72246cefd784395f9b068e8172e9f1292ef5
venkatasubbareddy66 <150311974+venkatasubbareddy66@users.noreply.github.com>
2024-02-06 16:16:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function submitForm() {
     const xhr = new XMLHttpRequest();
 
     // Set up request
-    xhr.open('POST', 'https://cqu8ksbd49.execute-api.ap-south-1.amazonaws.com/prod/register', true);
+    xhr.open('POST', 'https://r303hl3wx5.execute-api.ap-south-1.amazonaws.com/test/register', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     // Set up response handler
